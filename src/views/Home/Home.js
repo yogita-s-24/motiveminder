847e13e7c076c8aee806c10311a38c1d38adbebb
yogitashete2412 <yogitashete24@gmail.com>
2023-09-20 11:51:39 +0530

    creacted folder structure

---
+++ src/views/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center">Motive Minder</h1>
+    </div>
+  );
+};
+
+export default Home;
