5522cc60077d7b24bba6c089015e402e76b3a58c
yogitashete2412 <yogitashete24@gmail.com>
2023-09-20 23:53:08 +0530

    mapping added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -10,47 +10,87 @@ const Home = () => {
             description: "Only in two days",
             priority: 'very high'
         },
-        {
-            id: 2,
-            title: 'Go to Market',
-            description: "Buy Potato",
-            priority: 'very high',
-        },
-        {
-            id: 3,
-            title: 'Gappa Arti',
-            description: "Speak ganapati Aarti",
-            priority: ' very high'
-        },
-        {
-            id: 4,
-            title: 'Learn Tailwind Css',
-            description: "In three days",
-            priority: 'very high'
-        },
-
     ])
-    return (
-        <div className="container">
-            <h1 className="app-title">🎯 Motive Minder 🎯</h1>
-            <div className="todo-flex-container">
-                <div>
-                    <h2 className="text-center">Show List</h2>
-                    {/* Showing the list */}    
-                    {
-                        taskList.map((taskItem, index)=>{
-                            const {id,title,description,priority} = taskItem;
-                            return <Task id={id} title={title} description={description} priority={priority}/>
-                        })
-                    }
-                </div>
 
-                <div>
-                    <h2 className="text-center">Add List</h2>
-                </div>
+   const [title,setTitle] = useState('');
+   const [description,setDescription] = useState('');
+   const [priority,setPriority] = useState('');
+
+    const addTaskToList = () =>{
+
+        const randomId = Math.floor(Math.random() * 1000);
+      
+          const obj = {
+              id: randomId,
+              title:title,
+              description:description,
+              priority:priority
+          }
+      
+          setTaskList([...taskList, obj])
+      
+          setTitle('');
+          setDescription('');
+          setPriority('');
+    }
+ 
+
+    return (
+          <>
+            <div className="container">
+              <h1 className="app-title">🎯 Motive Minder 🎯</h1>
             </div>
-        </div>
-    );
-};
+            <div className="todo-flex-container">
+              <div>
+                <h1 className="text-center">Show List</h1>
+                {taskList.map((taskItem, index) => {
+                  const { id, title, description, priority } = taskItem;
+      
+                  return (
+                    <Task
+                      id={id}
+                      title={title}
+                      description={description}
+                      priority={priority}
+                    />
+                  );
+                })}
+              </div>
+      
+              <div>
+                <h1 className="text-center">Add List</h1>
+                <div className="add-task-container">
+                  <form action="">
+                   
+                    <input
+                      type="text"
+                      value={title}
+                      onChange={(e) => setTitle(e.target.value)}
+                      placeholder="Enter Task Here"
+                      className="text-input"
+                    />
+                    <input
+                      type="text"
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                      placeholder="Enter Description  Here"
+                      className="text-input"
+                    />
+                    <input
+                      type="text"
+                      value={priority}
+                      onChange={(e) => setPriority(e.target.value)}
+                      placeholder="Enter Priority Here"
+                      className="text-input"
+                    />
+                    <button type="button" className="btn-add-task" onClick={addTaskToList}>Add Task to List</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      }
+
 
 export default Home;
