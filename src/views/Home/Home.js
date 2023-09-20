e828da35b70149155c2061e7c3b3851082815011
yogitashete2412 <yogitashete24@gmail.com>
2023-09-20 11:59:16 +0530

    feat : created to two containers

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -4,7 +4,13 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="container">
-      <h1 className="text-center">Motive Minder</h1>
+      <h1 className="text-center">🎯 Motive Minder 🎯</h1>
+      <div className="toapp-container">
+
+
+      <div className="">Show List</div>
+      <div className="">Add List</div>
+      </div>
     </div>
   );
 };
